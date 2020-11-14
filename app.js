e7e631608586d390cac10cea4e7793066b74a995
emacliam <mushyp1608@gmail.com>
2020-11-14 17:15:36 +0200

    snd com

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
-/* app entry points */
-
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const { mongoUrl } = require('./config');
+const bodyParser = require('body-parser');
 
-// routes
-app.use((req, res, next) => {
-    res.status(200).json({
-        message: 'Hello world!!!'
-    });
-});
+//routes
+const authorsRoutes = require('./api/routes/authors');
+const booksRoutes = require('./api/routes/books');
+const bodyParser = require('body-parser');
+const config = require('./config/init');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(config.cors);
+
+app.use('/authors', authorsRoutes);
+app.use('/books', booksRoutes);
 
 module.exports = app;
